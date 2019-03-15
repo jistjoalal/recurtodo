942657de4b54a5eaf391bc65150d764b7dc5db38
jist <jistjoalal@gmail.com>
2019-03-14 18:08:25 -0700

    refactor addtodo component

--- imports/ui/AddTodo.jsx
+++ imports/ui/AddTodo.jsx
@@ -11,57 +11,16 @@ export default class AddTodo extends React.Component {
     };
   }
   render() {
-    const { text, num, time, err } = this.state;
     return (
       <form className="bg-light p-2" onSubmit={this.submit}>
 
         <h2>New Todo</h2>
 
-        {err &&
-          <p className="alert alert-warning">
-            {err}
-          </p>
-        }
+        { this.renderError() }
 
-        <div className="form-group">
-          <label>Todo</label>
-          <input
-            className="form-control"
-            type="text"
-            ref="text"
-            placeholder="Todo"
-            value={text}
-            onChange={this.change('text')}
-          />
-        </div>
-        
-        <div className="form-group">
-          <label>#</label>
-          <input
-            className="form-control"
-            type="number"
-            ref="num"
-            placeholder="#"
-            value={num}
-            min={1}
-            onChange={this.change('num')}
-          />
-        </div>
+        { this.renderTextInput() }
 
-        <div className="form-group">
-          <label>time</label>
-          <select
-            className="form-control"
-            ref="time"
-            value={time}
-            onChange={this.change('time')}
-          >
-            <option value="day">Day(s)</option>
-            <option value="week">Week(s)</option>
-            <option value="month">Month(s)</option>
-            <option value="year">Year(s)</option>
-          </select>
-        </div>
+        { this.renderDateInput() }
 
         <button className="btn btn-dark" type="submit">
           Submit
@@ -69,7 +28,65 @@ export default class AddTodo extends React.Component {
 
       </form>
     )
-  } 
+  }
+  renderError() {
+    return this.state.err && (
+      <p className="alert alert-warning">
+        {this.state.err}
+      </p>
+    );
+  }
+  renderTextInput() {
+    return (
+      <div className="form-group">
+        <label>Todo</label>
+        <input
+          className="form-control"
+          type="text"
+          ref="text"
+          placeholder="Todo"
+          value={this.state.text}
+          onChange={this.change('text')}
+        />
+      </div>
+    );
+  }
+  renderDateInput() {
+    return (
+      <div className="form-group">
+
+        <label>Due in:</label>
+        
+        <div className="d-flex">
+
+          <input
+            className="form-control"
+            type="number"
+            ref="num"
+            placeholder="#"
+            value={this.state.num}
+            min={1}
+            onChange={this.change('num')}
+          />
+
+          <select
+            className="form-control"
+            ref="time"
+            value={this.state.time}
+            onChange={this.change('time')}
+          >
+            <option value="day">Day(s)</option>
+            <option value="week">Week(s)</option>
+            <option value="month">Month(s)</option>
+            <option value="year">Year(s)</option>
+
+          </select> 
+          
+        </div>
+
+      </div>
+    )
+  }
   submit = e => {
     e.preventDefault();
     const { text, num, time } = this.state;
