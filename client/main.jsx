9334c17895f11d2d52979f28fc1dee2f6f06635f
jist <jistjoalal@gmail.com>
2019-03-28 18:20:59 -0700

    add pwa install button

--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,18 @@ import App from '/imports/ui/App'
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
+import InstallButton from './InstallButton';
 
 export default class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
 
         <div className="navbar bg-secondary text-dark">
           <h1 className="text-nowrap text-monospace">Do _, Due _.</h1>
+          <InstallButton />
         </div>
 
         <div className="container bg-dark p-2">
